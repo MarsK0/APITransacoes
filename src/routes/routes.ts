8dd0f8b544585fa73c542f60346a1afd35210232
MarsKO <marcosilhaag31@gmail.com>
2023-03-21 19:50:43 -0300

    Finished

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -14,7 +14,7 @@ routes.delete('/users/:id', (req, res) => bank.deleteUserById(req, res))
 routes.post('/user/:userid/transactions', checkUser, (req, res) => bank.postTransaction(req, res))
 routes.get('/user/:userid/transactions/:id', checkUser, (req, res) => bank.getTransaction(req, res))
 routes.get('/users/:userid/transactions', checkUser, (req, res) => bank.getTransactions(req, res))
-routes.put('/users/:userid/transactions/:id:', checkUser, (req, res) => bank.putTransactionById(req, res))
-routes.delete('/users/:userid/transactions/:id:', checkUser, (req, res) => bank.deleteTransactionById(req, res))
+routes.put('/users/:userid/transactions/:id', checkUser, (req, res) => bank.putTransactionById(req, res))
+routes.delete('/users/:userid/transactions/:id', checkUser, (req, res) => bank.deleteTransactionById(req, res))
 
 export default routes
